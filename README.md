9f749a38d9509f46a67f80acf077aa9c931ee06a
Akeem Mudashiru <akeemmudash@gmail.com>
2023-07-06 03:32:53 +0100

    printf REDO

---
+++ README.md
@@ -0,0 +1,3 @@
+#Custom made Printf program
+
+## ALX
---
+++ main.h
@@ -0,0 +1,75 @@
+#include "main.h"
+
+/**
+  * _printf - Imitates the functionality of the C's printf function.
+  * @format: A formatted string.
+  * Return: The number of bytes written.
+  */
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int i = 0, size = 0;
+	var_str *sp_var;
+	char c[2] = {0}, buffer[BUFF_SIZE] = {0};
+
+	if (!format || (format[0] == '%' && !format[1]))
+		return (-1);
+	
+	va_start(args, format);
+
+	while (format && format[i])
+	{
+		if (format[i] == '%')
+		{
+			sp_var = check_specifier(&format[i], args);
+			if (sp_var && sp_var->string)
+			{
+				check_buffer(buffer, sp_var->string);
+				size += strlen(sp_var->string);
+				i += (sp_var->i + 1);
+				free(sp_var->string);
+				free(sp_var);
+				continue;
+			}
+		}
+		c[0] = format[i];
+		check_buffer(buffer, c);
+		size++;
+		i++;
+	}
+	va_end(args);
+	write(1, buffer, strlen(buffer));
+	return (size);
+}
+
+/**
+ * check_specifier - Checks for special format specifiers.
+ * @format: Pointer to the format specifier character.
+ * @args: Arguments list.
+ *
+ * Return: A pointer to a structure containing the formatted string
+ *         and the length of the specifier, or NULL if no special specifier is found.
+ */
+var_str *check_specifier(const char *format, va_list args)
+{
+	/* Implementation of check_specifier function */
+}
+
+/**
+ * check_buffer - Checks if there is enough space in the buffer and appends a new string.
+ * @buffer: Buffer
+ * @str: New string
+ * Return: Number of bytes written to stdout.
+ */
+void check_buffer(char *buffer, char *str)
+{
+	int buf_len = strlen(buffer);
+	int str_len = strlen(str);
+
+	if (buf_len + str_len >= BUFF_SIZE)
+	{
+		write(1, buffer, buf_len);
+		buffer[0] = '\0';
+	}
+	strcat(buffer, str);
+}
